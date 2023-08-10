976352c19f59fabb9ddd12917b956815460092c6
soenghakk3301 <soenghakkchoeurn3301@gmail.com>
2023-08-10 14:11:02 +0700

    init vue api laravel

---
+++ todo-list-app/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Footer from './components/Footer.vue'
+import Navbar from './components/Navbar.vue'
+import TasksPage from './pages/TasksPage.vue'
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <TasksPage />
+    <Footer />
+  </div>
+</template>
+
+<style scoped></style>
